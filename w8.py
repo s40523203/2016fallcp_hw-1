0bd67d74e32c7d2f466ffa25294b566628431a8d
40523244 <40523244@gm.nfu.edu.tw>
2016-11-21 15:50:12 +0800

    40523244

--- w8.py
+++ w8.py
@@ -5,16 +5,16 @@ group = data2.splitlines()
 #print(group[0])
 result_g = []
 for i in group:
-	#print(i)
-	#print(i.split(",")[:-1])
-	for j in i.split(",")[:-1]:
-		result_g .append(j)
-print(result_g)
+    #print(i)
+    #print(i.split(",")[:-1])
+    for j in i.split(",")[:-1]:
+        result_g .append(j)
+#print(result_g)
 
 data3 = open("w6_list.txt").read()
 registered = data3.splitlines()
-print(registered)
+#print(registered )
 
 for m in registered:
-	if m not in result_g:
-		print(m)
+    if m not in result_g:
+        print(m)
